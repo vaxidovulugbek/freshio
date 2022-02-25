f9fd347b99fa7f82c9d3b42b03a7c6c5904eae00
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-02-25 08:40:15 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -5,11 +5,74 @@ let closeBt = document.getElementById("closeBtn");
 sidebarOpe.addEventListener("click", function(){
   sidebarModa.classList.add("shw");
   sidebarModa.classList.remove("shww")
-
 })
-
 
 closeBt.addEventListener("click", function(){
   console.log('ok');
   sidebarModa.classList.add("shww")
+})
+
+
+
+
+
+
+
+
+
+
+
+let sidebarMod = document.getElementById("sidemodal");
+let sidebarOp = document.getElementById("sideOpen");
+let closeB = document.getElementById("closBtn");
+
+sidebarOp.addEventListener("click", function(){
+  sidebarMod.classList.add("shw");
+  sidebarMod.classList.remove("shww")
+})
+
+closeB.addEventListener("click", function(){
+  console.log('ok');
+  sidebarMod.classList.add("shww")
+})
+
+
+
+
+
+
+
+
+
+
+let sidebarMo = document.getElementById("sidemoda");
+let sidebarO = document.getElementById("sideOpend");
+let closes = document.getElementById("closButtonn");
+
+sidebarO.addEventListener("click", function(){
+  sidebarMo.classList.add("shw");
+  sidebarMo.classList.remove("shww")
+})
+
+closes.addEventListener("click", function(){
+  console.log('ok');
+  sidebarMo.classList.add("shww")
+})
+
+
+
+
+
+let sidebarM = document.getElementById("sedemodales");
+let sidebar = document.getElementById("sideOpendBtn");
+let closes2 = document.getElementById("closebt");
+
+sidebar.addEventListener("click", function(){
+  sidebarM.classList.add("shw");
+  sidebarM.classList.remove("shww")
+})
+
+closes2.addEventListener("click", function(){
+  console.log('ok');
+  sidebarM.classList.add("shww")
 })
